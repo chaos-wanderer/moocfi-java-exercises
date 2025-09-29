fc1b86052294c682c6edfe1bedfc1df0b8bec18d
Johnlyod P. Robles <mrhumble1772@gmail.com>
2025-09-29 18:00:05 +0800

    commit test 2

--- mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Program.java
+++ mooc-java-programming-i/part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
+        System.out.println("test");
         // You can test your program here
 
         // another test for commit
