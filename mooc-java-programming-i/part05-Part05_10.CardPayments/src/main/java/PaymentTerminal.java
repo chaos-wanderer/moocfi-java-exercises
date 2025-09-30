fc588ef7b9b50ad4c5f4b6ed226127345e65beee
Johnlyod P. Robles <mrhumble1772@gmail.com>
2025-09-30 21:41:34 +0800

    completed

--- mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -6,24 +6,95 @@ public class PaymentTerminal {
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
+    private static final double AFFORDABLE_MEAL_PRICE = 2.50;
+    private static final double HEARTY_MEAL_PRICE = 4.30;
+
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
+        double AFFORDABLE_MEAL_PRICE = 2.50;
+
+        if (payment < AFFORDABLE_MEAL_PRICE) {
+            return payment;
+        }
         // increase the amount of cash by the price of an affordable mean and return the change
+        this.money += AFFORDABLE_MEAL_PRICE;
+        affordableMeals++;
+
+        return (payment - AFFORDABLE_MEAL_PRICE);
+
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+
     }
+
+    public boolean eatAffordably(PaymentCard card) {
+        // an affordable meal costs 2.50 euros
+
+        if (card.balance() < AFFORDABLE_MEAL_PRICE) {
+            return false;
+        }
+        // increase the amount of cash by the price of an affordable mean and return the change
+        // this.money += AFFORDABLE_MEAL_PRICE;
+        card.takeMoney(AFFORDABLE_MEAL_PRICE);
+        affordableMeals++;
+
+        return true;
+
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+
+    }
+
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
+
+        if (payment < HEARTY_MEAL_PRICE) {
+            return payment;
+        }
+
+        this.money += HEARTY_MEAL_PRICE;
+        heartyMeals++;
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        return (payment - HEARTY_MEAL_PRICE);
     }
+
+    public boolean eatHeartily(PaymentCard card) {
+        // a hearty meal costs 4.30 euros
+
+        double HEARTY_MEAL_PRICE = 4.30;
+
+        if (card.balance() < HEARTY_MEAL_PRICE) {
+            return false;
+        }
+
+        // this.money += HEARTY_MEAL_PRICE;
+        card.takeMoney(HEARTY_MEAL_PRICE);
+        heartyMeals++;
+        
+        // increase the amount of cash by the price of a hearty mean and return the change
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+        return true;
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum < 0) {
+            return;
+        }
+
+        card.addMoney(sum);
+        this.money += sum;
+    }
+
+
+
 
 
     @Override
