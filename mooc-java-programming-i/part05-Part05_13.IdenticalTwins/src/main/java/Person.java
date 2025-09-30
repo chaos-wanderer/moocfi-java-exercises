fa11ea011b97e7cc25107fcec64359654764d803
Johnlyod P. Robles <mrhumble1772@gmail.com>
2025-10-01 00:30:12 +0800

    completed

--- mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -15,5 +15,33 @@ public class Person {
         this.weight = weight;
     }
 
+    public boolean equals(Object compared) {
+        // if the variables are located in the same position, they are equal
+        if (this == compared) {
+            return true;
+        }
+
+        // if the type of the compared object is not SimpleDate, the objects are not equal
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        // convert the Object type compared object
+        // into a SimpleDate type object called comparedSimpleDate
+        Person comparedPerson = (Person) compared;
+
+        // if the values of the object variables are the same, the objects are equal
+        if (this.name.equals(comparedPerson.name) &&
+            this.birthday.equals(comparedPerson.birthday) &&
+            this.height == comparedPerson.height &&
+            this.weight == comparedPerson.weight) {
+            return true;
+        }
+
+        // otherwise the objects are not equal
+        return false;
+    }
+
+
     // implement an equals method here for checking the equality of objects
 }
