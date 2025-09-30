61e7e4e328b2db3958e1eda9f93db329c5e43966
Johnlyod P. Robles <mrhumble1772@gmail.com>
2025-10-01 00:29:48 +0800

    completed

--- mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
+++ mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
@@ -12,6 +12,34 @@ public class Song {
     }
 
     @Override
+    public boolean equals(Object compared) {
+        // if the variables are located in the same position, they are equal
+        if (this == compared) {
+            return true;
+        }
+
+        // if the type of the compared object is not SimpleDate, the objects are not equal
+        if (!(compared instanceof Song)) {
+            return false;
+        }
+
+        // convert the Object type compared object
+        // into a SimpleDate type object called comparedSimpleDate
+        Song comparedSong = (Song) compared;
+
+        // if the values of the object variables are the same, the objects are equal
+        if (this.artist.equals(comparedSong.artist) &&
+            this.name.equals(comparedSong.name) &&
+            this.durationInSeconds == comparedSong.durationInSeconds) {
+            return true;
+        }
+
+        // otherwise the objects are not equal
+        return false;
+    }
+
+
+    @Override
     public String toString() {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
     }
