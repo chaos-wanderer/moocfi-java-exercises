5a58d39a233e33732d0e7858a4f3b0392a90dd3f
Johnlyod P. Robles <mrhumble1772@gmail.com>
2025-10-01 14:31:55 +0800

    completed

--- mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
+++ mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
@@ -17,4 +17,36 @@ public class Book {
         return publicationYear;
     }
 
+    @Override
+    public boolean equals(Object comparedObject) {
+        // if the variables are located in the same place, they're the same
+        if (this == comparedObject) {
+            return true;
+        }
+
+        // if comparedObject is not of type Book, the objects aren't the same
+        if (!(comparedObject instanceof Book)) {
+            return false;
+        }
+
+	
+	//if (getClass() != obj.getClass())
+            //return false;
+
+        // let's convert the object to a Book-object
+        Book comparedBook = (Book) comparedObject;
+
+        // if the instance variables of the objects are the same, so are the objects
+        if (this.name.equals(comparedBook.name) &&
+            this.publicationYear == comparedBook.publicationYear) {
+            return true;
+        }
+
+        // otherwise, the objects aren't the same
+        return false;
+    }
+
+
+    
+
 }
