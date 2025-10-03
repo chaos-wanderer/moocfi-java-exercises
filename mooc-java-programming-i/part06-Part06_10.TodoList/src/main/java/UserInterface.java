0b2a3443631dc4093adba103910845eae44f6e47
Johnlyod P. Robles <mrhumble1772@gmail.com>
2025-10-03 20:13:40 +0800

    completed

---
+++ mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/UserInterface.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private TodoList list;
+    private Scanner scanner;
+
+    public UserInterface(TodoList list, Scanner scanner) {
+        this.list = list;
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("stop")) {
+                break;
+            }
+
+            processCommand(command);
+        }
+    }
+
+    private void processCommand(String command) {
+        
+        if (command.equals("add")) {
+            System.out.print("To add: ");
+            String task = scanner.nextLine();
+
+            this.list.add(task);
+
+        } else if (command.equals("list")) {
+            this.list.print();
+            
+        } else if (command.equals("remove")) {
+            System.out.print("Which one is removed? ");
+            int num = Integer.valueOf(scanner.nextLine());
+            this.list.remove(num);
+
+        } else {
+            System.out.println("Unknown command");
+        }
+    }
+    
+    
+}
